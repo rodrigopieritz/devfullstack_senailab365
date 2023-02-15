7e168068e6b9a17c313e6d7c1602d8ffbcc91b74
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-02-15 12:35:12 -0300

    add prompt with client's change

--- exercicios/PadariaTechPlus/functions.js
+++ exercicios/PadariaTechPlus/functions.js
@@ -1,18 +1,24 @@
 let valueList = [];
+let totalValue = 0;
 function itemRegister() {
   let itemPrice = parseFloat(document.getElementById("itemPrice").value);
   if (itemPrice != 0) {
     let displayItens = document.getElementById("displayItens");
     let displaySum = document.getElementById("displaySum");
     valueList.push(itemPrice);
-    displayItens.innerHTML = valueList.forEach((mercadoria)=>{mercadoria});
-    let totalValue = valueList.reduce((total, quantidade) => {
+    displayItens.innerHTML = valueList
+     let totalValue = valueList.reduce((total, quantidade) => {
       return (total = total + quantidade);
     });
     displaySum.innerHTML = "TOTAL: R$" + totalValue;
   } else {
-    alert(
-      "Digite o total de dinheiro entregue para que o troco seja calculado"
-    );
+   let money = 0;
+   let totalValue = valueList.reduce((total, quantidade) => {
+    return (total = total + quantidade);});
+   let change = 0;
+   money = parseFloat (prompt("Digite o valor total de dinehiro entregue, para que seja feito o cálculo do troco"));
+   change = money - totalValue;
+   alert( "seu troco é de R$ " + change);
+   //substituir vírgula por ., prompt ( "Digite seu número" ).replace ( ",", ".") )
   }
 }
