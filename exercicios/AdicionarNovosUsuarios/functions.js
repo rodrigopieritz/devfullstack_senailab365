3e16fdc467b8d44a16f857648bb9ec46672b1116
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-03-04 18:19:45 -0300

    1st commit

---
+++ exercicios/AdicionarNovosUsuarios/functions.js
@@ -0,0 +1,32 @@
+let lista = ["Pedro", "José", "Aderbal", "Danilo", "Luisa", "Vitoria"];
+let listaAdd = [];
+
+function adicionar(listaAdd) {
+  let tempList = [];
+  if (listaAdd.length < 1) {
+    console.log("insira um ou mais valores");
+  } else {
+    for (let i = 0; i < listaAdd.length; i++) {
+      if (lista.indexOf(listaAdd[i]) > -1) {
+        console.log("Erro! Não podem ser inseridos valores repetidos");
+        break;
+      } else if (typeof listaAdd[i] != "string") {
+        console.log("Erro! Só podem ser enviados nomes do tipo texto");
+        break;
+      } else {
+        tempList.push(listaAdd[i]);
+      }
+    }
+  }
+  if (tempList.length == listaAdd.length) {
+    tempList.map((elemento) => {
+      lista.push(elemento);
+    });
+    console.log(lista);
+  } else {
+    console.log("Arrume a lista e tente novamente");
+  }
+}
+
+let listaEnviada = ["Carlos", "Danilo","Luiz"];
+adicionar(listaEnviada);
