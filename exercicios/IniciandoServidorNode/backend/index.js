589b4f2ffee68d23f0561a399e10789ccb0869d3
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-03-11 20:20:04 -0300

    add exercice 4 codes

--- exercicios/IniciandoServidorNode/backend/index.js
+++ exercicios/IniciandoServidorNode/backend/index.js
@@ -6,6 +6,13 @@ app.get('/', (request, response) => {
   response.send("servidor está funcionando");
 });
 
+// exercício 4
+app.get('/enviar/:nome', (request, response) => {
+  let nome = request.params.nome;
+  response.send(`Rota de API criada pelo: ${nome}`);
+  console.log(`Console - Rota de API criada pelo: ${nome}`);
+});
+
 app.listen(3333, () => {
   console.log("servidor está funcionando!");
 });
