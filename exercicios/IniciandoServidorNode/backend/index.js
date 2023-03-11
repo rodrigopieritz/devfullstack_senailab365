b527539080e86a4f5ce3a0d0f0b030dc0431f14d
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-03-11 20:14:22 -0300

    add message

--- exercicios/IniciandoServidorNode/backend/index.js
+++ exercicios/IniciandoServidorNode/backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.send("servidor está funcionando");
+});
 
 app.listen(3333, () => {
-  console.log("servidor está funcionando (2)!");
+  console.log("servidor está funcionando!");
 });
