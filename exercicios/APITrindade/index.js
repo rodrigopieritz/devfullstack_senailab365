f1d87e040dcf7dd37ea2ec70dc9302d333502d6c
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-04-03 17:13:02 -0300

    exercice 2
    add database/ index.js + models/places.js

--- exercicios/APITrindade/index.js
+++ exercicios/APITrindade/index.js
@@ -1,5 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const connection = require('./src/database')
+
+const PLace = require('./src/models/place')//ex2
+const app = express()
+
+connection.authenticate()
+connection.sync({alter: true})//ex2
 
 app.listen(3333,()=>{
     console.log("servidor rodando na porta 3333")
