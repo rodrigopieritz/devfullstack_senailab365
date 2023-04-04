a4a361e92147d2b0d221797187d0b0f6fa68bd34
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-04-04 15:58:15 -0300

    ex. 6

--- exercicios/APITrindade/index.js
+++ exercicios/APITrindade/index.js
@@ -54,6 +54,32 @@ app.delete("/places/:id", async (request, response) => {
   }
 });
 
+app.put("/places/:id", async (request, response) => {
+  try {
+    const placeIndDatabase = await Place.findByPk(request.params.id);
+
+    if (!placeIndDatabase) {
+      return response
+        .status(404)
+        .json({ message: "Local não encontrado, informe um ID válido" });
+    }
+    placeIndDatabase.name = request.body.name;
+    placeIndDatabase.contact = request.body.contact;
+    placeIndDatabase.opening_hours = request.body.opening_hours;
+    placeIndDatabase.description = request.body.description;
+    placeIndDatabase.latitude = request.body.latitude;
+    placeIndDatabase.longitude = request.body.longitude;
+
+    await placeIndDatabase.save();
+    response.json(placeIndDatabase);
+  } catch (error) {
+    console.log(error);
+    response
+      .status(500)
+      .json({ message: "Não foi possivel concluir a operação, verificar log"});
+  }
+});
+
 app.listen(3333, () => {
   console.log("Servidor rodando na porta 3333");
 });
