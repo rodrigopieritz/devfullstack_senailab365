84ceea4b542f9a7cccae85a6b0e0495ea8cf53e5
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-06-30 20:50:46 -0300

    ex7

--- exercicios/M02/M02.S6_projetoVite/src/pages/NotFoundPage/NotFoundPage.jsx
+++ exercicios/M02/M02.S6_projetoVite/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,18 @@
+import React, { useContext, useEffect } from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
 export const NotFoundPage = () => {
-    return (
-      <>
-Erro 404 - Page is not found
-      </>
-    );
-  };
+  return (
+    <>
+      <Header />
+      <div class="image mx-auto text-center">
+        <img
+          src="https://img.freepik.com/vetores-gratis/conceito-de-erro-404-para-a-pagina-de-destino_52683-12188.jpg?w=740&t=st=1688168992~exp=1688169592~hmac=8241c34653071aa5166f1451eafcf6172aa9c18f6a51ec2b77ddb8650dad7156"
+          class="productImage"
+        ></img>
+      </div>
+      <Footer />
+    </>
+  );
+};
